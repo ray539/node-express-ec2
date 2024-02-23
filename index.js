5f609a68c6d8a02c55a9a993cfbc9c5d7cb598d0
Ray <ray.y.wang@student.unsw.edu.au>
2024-02-23 11:44:27 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express()
+app.listen(5001, () => {
+  console.log('api running on port 5001')
+})
+
+app.get('/', (req, res) => {
+  res.json('API is running :)')
+})
